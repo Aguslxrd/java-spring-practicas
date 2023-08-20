6459b818f4fbb703bc7465966286bb8e0b21e9ca
damian <suffodamian02@gmail.com>
2023-08-20 05:03:59 -0300

    agregado template modelo y controlador de usuario

--- src/main/java/com/practicasspring/practicasenspring/controllers/UsuarioController.java
+++ src/main/java/com/practicasspring/practicasenspring/controllers/UsuarioController.java
@@ -1,4 +1,22 @@
 package com.practicasspring.practicasenspring.controllers;
 
+import com.practicasspring.practicasenspring.models.Usuario;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UsuarioController {
+
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("damian");
+        usuario.setApellido("suffo");
+        usuario.setEmail("suffodamian02@gmail.com");
+        usuario.setTelefono("232323");
+        usuario.setPassword("123456789");
+        return usuario;
+    }
 }
---
+++ src/main/java/com/practicasspring/practicasenspring/models/Usuario.java
@@ -0,0 +1,50 @@
+package com.practicasspring.practicasenspring.models;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String Password;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+}
