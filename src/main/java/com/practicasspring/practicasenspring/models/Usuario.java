0257b3a979974c461d3f684ad07fb9925f661e4f
damian <suffodamian02@gmail.com>
2023-08-20 21:25:46 -0300

    lombok import para usar las anotaciones @Getter y @Setter

--- src/main/java/com/practicasspring/practicasenspring/models/Usuario.java
+++ src/main/java/com/practicasspring/practicasenspring/models/Usuario.java
@@ -1,61 +1,26 @@
 package com.practicasspring.practicasenspring.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
 @Table(name = "usuario")
 public class Usuario {
+
+    @Getter @Setter
     private long id;
+    @Getter @Setter
     private String nombre;
+    @Getter @Setter
     private String apellido;
+    @Getter @Setter
     private String email;
+    @Getter @Setter
     private String telefono;
+    @Getter @Setter
     private String Password;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
 
 
 }
